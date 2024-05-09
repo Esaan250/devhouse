ac720fff6964dcbc52b0409a5fb0ee43bd28d772
Esaan Alencar <esaanalencar250@gmail.com>
2024-05-09 17:25:16 -0300

    Arquivo "app.js" atualizado.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
 import express from "express";
 import routes from "./routes";
+import mongoose from "mongoose";
+import path from "path";
+const login = "";
+const password = "";
 class App {
   constructor() {
     this.server = express();
+    mongoose
+      .connect(
+        `mongodb+srv://${}:${password}@mongoserver.vc6jcnv.mongodb.net/?retryWrites=true&w=majority&appName=mongoserver`
+      )
+      .then((db) => {
+        console.log(db);
+      })
+      .catch((error) => {
+        console.log(db);
+      });
     this.middlewares();
     this.routes();
   }
   middlewares() {
+    this.server.use(
+      "/files",
+      express.static(path.resolve(__dirname, "..", "uploads"))
+    );
     this.server.use(express.json());
   }
   routes() {
