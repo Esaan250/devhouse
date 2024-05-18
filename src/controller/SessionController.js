060ef8cff14e5ac6a1f7f7a79a982c69285b5db5
Esaan Alencar <esaanalencar250@gmail.com>
2024-05-17 21:06:22 -0300

    "SessionController.js" atualizado.

--- src/controller/SessionController.js
+++ src/controller/SessionController.js
@@ -1,7 +1,14 @@
 import User from "../models/User";
+import * as Yup from "yup";
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+    });
     const { email } = req.body;
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ erro: "Falha na validação." });
+    }
     var user = await User.findOne({ email });
     if (!user) {
       user = await User.create({ email });
